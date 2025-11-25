28e13d3b8d7b71e4ec7e605cf407e5c496005f3f
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2025-11-24 23:20:09 -0300

    Feat: finish

--- UI/health_ui.gd
+++ UI/health_ui.gd
@@ -47,3 +47,6 @@ func _process(delta: float) -> void:
 		$HealthRepresentation.play("1")
 		
 		$Life/AnimatedSprite2D.play("half")
+		
+	elif start_life == 0:
+		get_tree().change_scene_to_file("res://game_over.tscn")
